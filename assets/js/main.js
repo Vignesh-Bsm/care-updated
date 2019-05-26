bae7bc4c8531f1cd3ee5076418c120b9e22955bd
Vignesh-Bsm <42670004+Vignesh-Bsm@users.noreply.github.com>
2019-05-26 22:21:00 +0530

    updated
    closed-spacing issues

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$('.slider').slick({
+    vertical: true,
+    autoplay: true,
+    autoplaySpeed: 800,
+    speed: 1000,
+    transition : 'opacity 10s ease-in-out',
+    dots: false,
+    prevArrow: false,
+    nextArrow: false
+  });
